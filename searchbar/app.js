64aa92a4951057cee757d67b08a0552e97cb8c55
Esther Kleikers <46675234+EKleikers@users.noreply.github.com>
2025-12-30 12:12:31 +0100

    getAutoCompleteSuggestions() - full functionality incl .css

--- searchbar/app.js
+++ searchbar/app.js
@@ -1,6 +1,7 @@
 
 const searchInput = document.querySelector("#search");
 const suggestionContainer = document.querySelector(".suggestions");
+const suggestionWrap = document.querySelector('.suggestion-wrap');
 const searchContainer = document.querySelector(".search-container");
 
 // Single-file MVC-style structure for demo purposes.
@@ -11,42 +12,83 @@ const model = {
 
 	async loadData() {
 
-		console.log("model.LoadData()");
-		const response = await fetch("data.json");
+		const response = await fetch("/././data.json");
 		this.data = await response.json();
 	},
 
 	getAutocompleteSuggestions() {
-		console.log("model.getAutocompleteSuggestions()");
-		const suggestionsList = this.data;
-		let searchValue = searchInput.value;
-		let recommendedList = [];
-		if (searchValue.length) {
-			recommendedList = suggestionsList.filter(listItems => listItems.tag.includes(searchValue));
+
+		const searchValue = helpers.normalize(searchInput.value);
+		// start autocomplate after 3rd character in searchbar
+		if (searchValue.length < 3) {
+			view.clear(); 
+			return;
 		}
-		view.showRecommendedList(recommendedList);
-	},
+		// flatten all tags
+		const allTags = this.data.flatMap(item => item.tags);
+
+		// make unique
+		const uniqueTags = [...new Set(allTags)];
+
+		// filter per tag
+		let filteredTags = uniqueTags.filter(tag =>
+			helpers.normalize(tag).includes(searchValue)
+		);
+
+		// sort alphabetically
+		filteredTags.sort();
+
+		// limit number list items
+		filteredTags = filteredTags.slice(0, 5);
+
+		view.showTagSuggestions(filteredTags);
+	}
 };
 
 const view = {
+	showTagSuggestions(list) {
+		console.log("view.showTagSuggestions()");
+		this.clear();
 
-	showRecommendedList(list) {
-		console.log("view.showRecommendedList()");
-		searchContainer.classList.add("show");
+		if (!list.length) {
+			this.clear();
+			return;
+		} else {
+			suggestionWrap.classList.add("show");
+		}
 
 		list.forEach(listItem => {
 			const li = document.createElement("li");
-			li.textContent = listItem.tag;
+			li.textContent = listItem;
+			li.addEventListener("click", () => {
+				searchInput.value = listItem;
+				this.clear();
+			});
 			suggestionContainer.appendChild(li);
 		})
+	},
+
+	// empty and hide listitems
+	clear() {
+		suggestionContainer.innerHTML = "";
+		searchContainer.classList.remove("show");
 	},
 };
 
 const controller = {
 	handleKeyup() {
-		console.log("controller.handelKeyop()");
 		model.getAutocompleteSuggestions();
 	},
+};
+
+const helpers = {
+	// case & diacritics insensitive
+	normalize(str) {
+		return str
+			.toLowerCase()
+			.normalize("NFD")
+			.replace(/[\u0300-\u036f]/g, "");
+	}
 };
 
 async function init() {
